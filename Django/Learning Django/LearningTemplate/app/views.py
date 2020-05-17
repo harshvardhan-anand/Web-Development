725a74d9b437a889e92e6acefa77fe2777526c8a
Harshvardhan Anand <hharshvardhanaanand@gmail.com>
2020-05-17 23:53:45 +0530

    inserted text

--- Django/Learning Django/LearningTemplate/app/views.py
+++ Django/Learning Django/LearningTemplate/app/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(response):
     inserted_data = {
-        "inserted_location":"homepage",
-        "inserted_from":"app.views"
+        "inserted_to":"I am inserted to homepage.html",
+        "inserted_from":"I am inserted from app.views"
     }
     return render(response, "homepage.html", context=inserted_data)
 
