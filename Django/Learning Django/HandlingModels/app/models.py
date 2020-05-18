297d749c2d8aa5f3ca22d8c30a4eda16d924a28c
Harshvardhan Anand <hharshvardhanaanand@gmail.com>
2020-05-18 15:20:46 +0530

    Made Migrations

--- Django/Learning Django/HandlingModels/app/models.py
+++ Django/Learning Django/HandlingModels/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class PersonDetails(models.Model):
+    user_id = models.CharField(unique=True, primary_key=True, max_length=50)
+    name_of_person = models.TextField()
+    phone_no = models.IntegerField()
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+
